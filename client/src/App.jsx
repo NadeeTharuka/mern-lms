a10fcbd09fdb4b96b6a8d70f4c240c7d5533a275
Nadee Tharuka <nadeetharuka2020@gmail.com>
2024-11-01 22:47:39 +0530

    Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import AuthPage from "./pages/auth";
+import RouteGuard from "./components/route-guard";
+import { useContext } from "react";
+import { AuthContext } from "./context/auth-context";
+import InstructorDashboardpage from "./pages/instructor";
+import StudentViewCommonLayout from "./components/student-view/common-layout";
+import StudentHomePage from "./pages/student/home";
+import NotFoundPage from "./pages/not-found";
+import AddNewCoursePage from "./pages/instructor/add-new-course";
+import StudentViewCoursesPage from "./pages/student/courses";
+import StudentViewCourseDetailsPage from "./pages/student/course-details";
+import PaypalPaymentReturnPage from "./pages/student/payment-return";
+import StudentCoursesPage from "./pages/student/student-courses";
+import StudentViewCourseProgressPage from "./pages/student/course-progress";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { auth } = useContext(AuthContext);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route
+        path="/auth"
+        element={
+          <RouteGuard
+            element={<AuthPage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/instructor"
+        element={
+          <RouteGuard
+            element={<InstructorDashboardpage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/instructor/create-new-course"
+        element={
+          <RouteGuard
+            element={<AddNewCoursePage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/instructor/edit-course/:courseId"
+        element={
+          <RouteGuard
+            element={<AddNewCoursePage />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <RouteGuard
+            element={<StudentViewCommonLayout />}
+            authenticated={auth?.authenticate}
+            user={auth?.user}
+          />
+        }
+      >
+        <Route path="" element={<StudentHomePage />} />
+        <Route path="home" element={<StudentHomePage />} />
+        <Route path="courses" element={<StudentViewCoursesPage />} />
+        <Route
+          path="course/details/:id"
+          element={<StudentViewCourseDetailsPage />}
+        />
+        <Route path="payment-return" element={<PaypalPaymentReturnPage />} />
+        <Route path="student-courses" element={<StudentCoursesPage />} />
+        <Route
+          path="course-progress/:id"
+          element={<StudentViewCourseProgressPage />}
+        />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
